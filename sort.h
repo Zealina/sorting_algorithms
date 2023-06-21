b3384a72da623d0129f7938a748c2c57cbcfd4f0
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-06-21 12:51:55 +0000

    Delete the old ones

--- sort.h
+++ sort.h
@@ -21,5 +21,9 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+int partition(int *array, int hign, int low);
+void quick_sort_recurstion(int *array, int high, int low);
+void swap(int *x, int *y);
 
 #endif /* SORT_H */
