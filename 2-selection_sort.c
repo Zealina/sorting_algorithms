7b285412e97d4d93932ba0fdc3ff798ff0f40520
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-06-26 11:11:20 +0000

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,22 +9,25 @@
 void selection_sort(int *array, size_t size)
 {
 	int temp;
-	size_t i, j;
+	size_t i, j, min_index;
 
 	if (array == NULL || size <= 1)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
+		min_index = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
-			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				print_array(array, size);
-			}
+			if (array[min_index] > array[j])
+				min_index = j;
+		}
+		if (min_index != i)
+		{
+			temp = array[min_index];
+			array[min_index] = array[i];
+			array[i] = temp;
+			print_array(array, size);
 		}
 	}
 }
