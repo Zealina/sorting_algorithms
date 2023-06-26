b3d0735873305b338051be773a0f03b25dd6672a
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-06-26 10:24:16 +0000

    I figured out the problem

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,36 +1,38 @@
 #include "sort.h"
-#include <stdio.h>
-
 /**
- * insertion_sort_list - Sorts a doubly linked list using insertion
- *	sort algorithm
+ * insertion_sort_list - Sorts a doubly linked
+ *	list using insertion sort algorithm
  * @list: The list to be sorted
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *trav, *reset, *temp;
+	listint_t *current, *temp, *prev;
 
-	trav = *list;
-	while (trav != NULL && trav->next != NULL)
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current != NULL)
 	{
-		reset = trav->next;
-		while (reset->prev != NULL)
+		temp = current;
+		prev = current->prev;
+		while (prev != NULL && prev->n > temp->n)
 		{
-			temp = reset->prev;
-			if (reset->n < temp->n)
-			{
-				temp->next = reset->next;
-				reset->next = temp;
-				reset->prev = temp->prev;
-				temp->prev = reset;
-				if (temp->next != NULL)
-					temp->next->prev = temp;
-				if (reset->prev != NULL)
-					reset->prev->next = reset;
-				print_list(*list);
-			}
-			reset = reset->prev;
+			if (prev->prev != NULL)
+				prev->prev->next = temp;
+			if (temp->next != NULL)
+				temp->next->prev = prev;
+			prev->next = temp->next;
+			temp->prev = prev->prev;
+			temp->next = prev;
+			prev->prev = temp;
+			if (temp->prev == NULL)
+				*list = temp;
+			prev = temp->prev;
+			print_list(*list);
 		}
-		trav = trav->next;
+		current = current->next;
 	}
 }
---
+++ 2-selection_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts using the selection sort algorithm
+ * @array: The unsorted array
+ * @size: The size of @array
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+	;
